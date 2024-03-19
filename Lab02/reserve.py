e6306198dc39c0ab8f9eaf8c058ea79655836fad
Vincenzo Netti <vincenzontt02@gmail.com>
2024-03-19 19:52:21 +0100

    added nQueens exercise

--- Lab02/reserve.py
+++ Lab02/reserve.py
@@ -1,6 +1,6 @@
 from pysmt.shortcuts import *
 
-msat = Solver()
+msat = UnsatCoreSolver()
 formula = []
 vars = {"x{}{}".format(i,j): Symbol("x{}{}".format(i,j), BOOL) for i in "abcde" for j in range(1,6)}
 
@@ -31,6 +31,7 @@ for i in range(len(guests_conditions)):
 	if res:
 		pass
 	else:
+		print(msat.get_unsat_core())
 		print("We can satisfy in order {} people".format(i))
 		exit()
 
